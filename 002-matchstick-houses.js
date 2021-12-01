647f02196f428d2fa6f64848820a656995d3f8f8
Diego Castro <diegojcr4@gmail.com>
2021-11-30 22:43:49 -0500

    add instructions and link

--- 002-matchstick-houses.js
+++ 002-matchstick-houses.js
@@ -1,3 +1,9 @@
+/**
+ * Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step.
+ * A house uses six matchsticks and adjacent houses share exactly one matchstick with each other.
+ * @param {number} step the number of houses
+ * @returns {number} the number of matchsticks used
+ */
 function matchHouses(step) {
 	if (step === 0) return 0;
 	else if (step === 1) return (6);
