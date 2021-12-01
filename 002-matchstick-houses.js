50687f1cbbdc733b30eddbf89dd0626c6b361911
Diego Castro <diegojcr4@gmail.com>
2021-11-30 22:45:34 -0500

    improve code logic

--- 002-matchstick-houses.js
+++ 002-matchstick-houses.js
@@ -1,12 +1,11 @@
 /**
  * Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step.
  * A house uses six matchsticks and adjacent houses share exactly one matchstick with each other.
+ * https://edabit.com/challenge/tYHkTdFrEmWfxpPKF
  * @param {number} step the number of houses
  * @returns {number} the number of matchsticks used
  */
 function matchHouses(step) {
-	if (step === 0) return 0;
-	else if (step === 1) return (6);
-	else if (step === 2) return (11);
-	else return (step * 5 + 1)
+  if (step === 0) return 0;
+	return (step * 5 + 1)
 }
